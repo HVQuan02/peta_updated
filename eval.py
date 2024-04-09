1055a22d19a15b3af64da7a99146de007fae46b1
HVQuan02 <hovquan02@gmail.com>
2024-04-09 10:59:31 +0700

    update stuff in eval.py

--- eval.py
+++ eval.py
@@ -4,7 +4,6 @@ import torch
 from torch.utils.data import DataLoader
 from src.models import create_model
 from sklearn.metrics import average_precision_score, accuracy_score
-import numpy as np
 
 from datasets import CUFED
 
@@ -15,13 +14,13 @@ parser.add_argument('--num_classes', type=int, default=23)
 parser.add_argument('--dataset', default='cufed', choices=['cufed', 'pec', 'holiday'])
 parser.add_argument('--dataset_path', type=str, default='/content/drive/MyDrive/CUFED-Event-Image/CUFED')
 parser.add_argument('--dataset_type', type=str, default='ML_CUFED')
-parser.add_argument('--batch_size', type=int, default=64, help='batch size') # change
+parser.add_argument('--batch_size', type=int, default=32, help='batch size') # change
 parser.add_argument('--transform_type', type=str, default='squish')
 parser.add_argument('--album_sample', type=str, default='rand_permute')
 parser.add_argument('--num_workers', type=int, default=2, help='number of workers for data loader')
 parser.add_argument('--save_scores', action='store_true', help='save the output scores')
 parser.add_argument('--save_path', default='scores.txt', help='output path')
-# parser.add_argument('-v', '--verbose', action='store_true', help='show details')
+parser.add_argument('-v', '--verbose', action='store_true', help='show details')
 parser.add_argument('--img_size', type=int, default=224)
 parser.add_argument('--album_clip_length', type=int, default=32)
 parser.add_argument('--remove_model_jit', type=int, default=None)
@@ -35,11 +34,8 @@ def evaluate(model, dataset, loader, scores, out_file, device):
     model.eval()
     with torch.no_grad():
         for batch in loader:
-            # print(batch.shape)
             feats = batch.to(device)
             out_data = model(feats)
-            # print(out_data)
-            # print(out_data.shape)
             shape = out_data.shape[0]
 
             if out_file:
@@ -57,6 +53,10 @@ def main():
     dataset = CUFED(root_dir=args.dataset_path, is_train=False, img_size=args.img_size, album_clip_length=args.album_clip_length)
   device = torch.device('cuda:0')
   val_loader = DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers)
+
+  if args.verbose:
+    print("running on {}".format(device))
+    print("num samples={}".format(len(dataset)))
 
   out_file = None
   if args.save_scores:
@@ -82,7 +82,8 @@ def main():
     out_file.close()
 
   if args.dataset == 'cufed':
-    ap = average_precision_score(dataset.labels, scores)
+    ap = average_precision_score(dataset.labels, scores) 
+    # ap = accuracy_score(dataset.labels, scores)
     print('top1={:.2f}% dt={:.2f}sec'.format(100 * ap, t1 - t0))
 
 if __name__ == '__main__':
